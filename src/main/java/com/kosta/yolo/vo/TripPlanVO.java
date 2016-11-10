213917bdc114f894d16ae8ac0aacc0dd5deff678
heenim <hee20102@gmail.com>
2016-11-10 20:14:00 +0900

    Revert "Revert "설정잡앗어요~~~ㅎㅎ""
    This reverts commit 34e573a2f5f064f134c72dabb256a848fbd8e4b0.

---
+++ src/main/java/com/kosta/yolo/vo/TripPlanVO.java
@@ -0,0 +1,134 @@
+package com.kosta.yolo.vo;
+
+public class TripPlanVO {
+
+	//일정 짜기 
+	//사용자 ID, 여행지 이름, 여행지 가격,  내 메모
+	private String user_id, trip_id1, trip_pay1, trip_memo1;
+	private String user_id2, trip_id2, trip_pay2, trip_memo2;
+	private String user_id3, trip_id3, trip_pay3, trip_memo3;
+	private String user_id4, trip_id4, trip_pay4, trip_memo4;
+	private String user_id5, trip_id5, trip_pay5, trip_memo5;
+	
+	public String getUser_id() {
+		return user_id;
+	}
+	public void setUser_id(String user_id) {
+		this.user_id = user_id;
+	}
+	public String getTrip_id1() {
+		return trip_id1;
+	}
+	public void setTrip_id1(String trip_id1) {
+		this.trip_id1 = trip_id1;
+	}
+	public String getTrip_pay1() {
+		return trip_pay1;
+	}
+	public void setTrip_pay1(String trip_pay1) {
+		this.trip_pay1 = trip_pay1;
+	}
+	public String getTrip_memo1() {
+		return trip_memo1;
+	}
+	public void setTrip_memo1(String trip_memo1) {
+		this.trip_memo1 = trip_memo1;
+	}
+	public String getUser_id2() {
+		return user_id2;
+	}
+	public void setUser_id2(String user_id2) {
+		this.user_id2 = user_id2;
+	}
+	public String getTrip_id2() {
+		return trip_id2;
+	}
+	public void setTrip_id2(String trip_id2) {
+		this.trip_id2 = trip_id2;
+	}
+	public String getTrip_pay2() {
+		return trip_pay2;
+	}
+	public void setTrip_pay2(String trip_pay2) {
+		this.trip_pay2 = trip_pay2;
+	}
+	public String getTrip_memo2() {
+		return trip_memo2;
+	}
+	public void setTrip_memo2(String trip_memo2) {
+		this.trip_memo2 = trip_memo2;
+	}
+	public String getUser_id3() {
+		return user_id3;
+	}
+	public void setUser_id3(String user_id3) {
+		this.user_id3 = user_id3;
+	}
+	public String getTrip_id3() {
+		return trip_id3;
+	}
+	public void setTrip_id3(String trip_id3) {
+		this.trip_id3 = trip_id3;
+	}
+	public String getTrip_pay3() {
+		return trip_pay3;
+	}
+	public void setTrip_pay3(String trip_pay3) {
+		this.trip_pay3 = trip_pay3;
+	}
+	public String getTrip_memo3() {
+		return trip_memo3;
+	}
+	public void setTrip_memo3(String trip_memo3) {
+		this.trip_memo3 = trip_memo3;
+	}
+	public String getUser_id4() {
+		return user_id4;
+	}
+	public void setUser_id4(String user_id4) {
+		this.user_id4 = user_id4;
+	}
+	public String getTrip_id4() {
+		return trip_id4;
+	}
+	public void setTrip_id4(String trip_id4) {
+		this.trip_id4 = trip_id4;
+	}
+	public String getTrip_pay4() {
+		return trip_pay4;
+	}
+	public void setTrip_pay4(String trip_pay4) {
+		this.trip_pay4 = trip_pay4;
+	}
+	public String getTrip_memo4() {
+		return trip_memo4;
+	}
+	public void setTrip_memo4(String trip_memo4) {
+		this.trip_memo4 = trip_memo4;
+	}
+	public String getUser_id5() {
+		return user_id5;
+	}
+	public void setUser_id5(String user_id5) {
+		this.user_id5 = user_id5;
+	}
+	public String getTrip_id5() {
+		return trip_id5;
+	}
+	public void setTrip_id5(String trip_id5) {
+		this.trip_id5 = trip_id5;
+	}
+	public String getTrip_pay5() {
+		return trip_pay5;
+	}
+	public void setTrip_pay5(String trip_pay5) {
+		this.trip_pay5 = trip_pay5;
+	}
+	public String getTrip_memo5() {
+		return trip_memo5;
+	}
+	public void setTrip_memo5(String trip_memo5) {
+		this.trip_memo5 = trip_memo5;
+	}
+	
+}
---
+++ src/main/java/com/kosta/yolo/vo/UserVO.java
@@ -0,0 +1,41 @@
+package com.kosta.yolo.vo;
+
+public class UserVO {
+
+	//사용자(회원) 정보
+	private String id, password, email, gender;
+	private int age;
+	
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getGender() {
+		return gender;
+	}
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	
+}
